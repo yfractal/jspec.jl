04fe398b1c654f0229c0c4c4300aab407449e257
yfractal <mike@beansmile.com>
2014-10-21 08:38:38 +0800

    add befores, to save the assignments in befores macro

--- src/test.jl
+++ src/test.jl
@@ -1,9 +1,18 @@
 using Base.Test
+
+global context_counter = [0]
+befores = Dict();
+
 function context(f::Function, description)
+    push!(context_counter, context_counter[end] + 1)
+
     println(description)
     f()
 end
 
 macro before(exp::Expr)
+    esc(merge!(befores, {context_counter[end] => exp}))
+
+    println(befores)
     esc(exp)
 end
